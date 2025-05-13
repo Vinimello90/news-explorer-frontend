3234d8eb9021b00b3a674c4ec9da6ba9bc8e1175
vinimello90 <vinicius.barretto9022@gmail.com>
2025-05-13 11:24:34 -0300

    style: adjust header backdrop brightness and navigation layout for tablets

--- src/components/Header/components/Navigation/Navigation.jsx
+++ src/components/Header/components/Navigation/Navigation.jsx
@@ -4,30 +4,32 @@ import "./Navigation.css";
 export function Navigation() {
   return (
     <nav className="navigation">
-      <h1 className="navigation__logo">
-        <Link to="/" class="navigation__link">
-          NewsExplorer
-        </Link>
-      </h1>
-      <ul className="navigation__menu">
-        <li className="navigation__menu-item">
-          <NavLink
-            to="/"
-            className={({ isActive }) =>
-              isActive
-                ? "navigation__link navigation__link_active"
-                : "navigation__link"
-            }
-          >
-            Início
-          </NavLink>
-        </li>
-        <li className="navigation__menu-item">
-          <button type="button" className="navigation__button">
-            Entrar
-          </button>
-        </li>
-      </ul>
+      <div className="navigation__container">
+        <h1 className="navigation__logo">
+          <Link to="/" class="navigation__link">
+            NewsExplorer
+          </Link>
+        </h1>
+        <ul className="navigation__menu">
+          <li className="navigation__menu-item">
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive
+                  ? "navigation__link navigation__link_active"
+                  : "navigation__link"
+              }
+            >
+              Início
+            </NavLink>
+          </li>
+          <li className="navigation__menu-item">
+            <button type="button" className="navigation__button">
+              Entrar
+            </button>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
