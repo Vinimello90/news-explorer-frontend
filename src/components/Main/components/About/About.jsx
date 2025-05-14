93c8b95bbe6c547047761545b185cc0f98240409
vinimello90 <vinicius.barretto9022@gmail.com>
2025-05-14 16:02:04 -0300

    style(about): add styles to About layout

--- src/components/Main/components/About/About.jsx
+++ src/components/Main/components/About/About.jsx
@@ -1,10 +1,21 @@
+import photo from "../../../../images/placeholder.jpg";
+import "./About.css";
+
 export function About() {
   return (
     <section className="about">
-      <div className="about__container">
-        <img src="" alt="" className="about__author-photo" />
-        <h2 className="about__title"></h2>
-        <p className="about__description"></p>
+      <img src={photo} alt="" className="about__author-photo" />
+      <div className="about__content">
+        <h2 className="about__title">Sobre o autor</h2>
+        <p className="about__description">
+          Esse bloco descreve o autor do projeto. Aqui você deve indicar seu
+          nome, o que você faz e quais tecnologias de desenvolvedor você
+          conhece.
+        </p>
+        <p className="about__description">
+          Você também pode falar sobre sua experiência com o Practicum, o que
+          aprendeu lá e como pode ajudar clientes em potencial.
+        </p>
       </div>
     </section>
   );
