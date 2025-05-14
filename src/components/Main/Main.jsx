7609d3a97ffc353b70f42e20efd9ef5e3e397e24
vinimello90 <vinicius.barretto9022@gmail.com>
2025-05-14 13:52:24 -0300

    feat(main): add markup for About component

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,9 @@
+import { About } from "./components/About/About";
+
+export function Main() {
+  return (
+    <main className="main">
+      <About />
+    </main>
+  );
+}
---
+++ src/components/Main/components/About/About.jsx
@@ -0,0 +1,11 @@
+export function About() {
+  return (
+    <section className="about">
+      <div className="about__container">
+        <img src="" alt="" className="about__author-photo" />
+        <h2 className="about__title"></h2>
+        <p className="about__description"></p>
+      </div>
+    </section>
+  );
+}
