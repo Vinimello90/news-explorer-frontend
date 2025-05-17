c2f595be15383a305b690e3fa05ba7451b1c6e54
vinimello90 <vinicius.barretto9022@gmail.com>
2025-05-16 22:26:04 -0300

    style(popup): add styles to Popup component

--- src/components/Main/components/Popup/Popup.jsx
+++ src/components/Main/components/Popup/Popup.jsx
@@ -1,10 +1,18 @@
+import "./Popup.css";
+
 export function Popup({ popup }) {
   return (
     <div className="popup">
       <div className="popup__container">
-        <button className="popup__close_button"></button>
+        <button className="popup__button popup__close_button"></button>
         <h2 className="popup__title">{popup.title}</h2>
         {popup.children}
+        <p className="popup__signup-text">
+          ou{" "}
+          <button type="button" className="popup__button popup__button_goto">
+            Inscreva-se
+          </button>
+        </p>
       </div>
     </div>
   );
--- src/components/Main/components/Popup/components/PopupWithForm/PopupWithForm.jsx
+++ src/components/Main/components/Popup/components/PopupWithForm/PopupWithForm.jsx
@@ -1,14 +1,21 @@
 export function PopupWithForm({ signIn }) {
   return (
-    <form action="" className="popup__form">
+    <form action="" name="signin" className="popup__form">
       <fieldset className="popup__fieldset">
         <label className="popup__field">
+          E-mail
           <input type="email" name="email" className="popup__input" />
+          <span className="popup__form-error">ronaldo</span>
         </label>
         <label className="popup__field">
+          Password
           <input type="password" name="password" className="popup__input" />
+          <span className="popup__form-error">ronaldo</span>
         </label>
-        <button type="submit" className="popup__submit-button">
+        <span className="popup__form-error popup__form-error_submit">
+          ronaldo
+        </span>
+        <button type="submit" className="popup__button popup__button_submit">
           Entrar
         </button>
       </fieldset>
