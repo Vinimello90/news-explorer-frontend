5755f6ec9809aaa0c15309a4596194d4c4918b02
vinimello90 <vinicius.barretto9022@gmail.com>
2025-05-18 23:04:43 -0300

    feat(news): add markup for when no search results are found

--- src/components/Main/components/News/NewsCardList/NewsCardList.jsx
+++ src/components/Main/components/News/NewsCardList/NewsCardList.jsx
@@ -1,6 +1,7 @@
 import "./NewsCardList.css";
 import { NewsCard } from "../shared/NewsCard/NewsCard";
-import { useState } from "react";
+import notFound from "../../../../../images/not-found_v1.svg";
+import { useEffect, useState } from "react";
 
 export function NewsCardList({ articles }) {
   const [newsLimit, setCardsLimit] = useState(3);
@@ -9,23 +10,35 @@ export function NewsCardList({ articles }) {
     setCardsLimit(newsLimit + 3);
   }
 
+  if (articles.length > 0) {
+    return (
+      <>
+        <h2 className="news__title">Procurar resultados</h2>
+        <ul className="news__card-list">
+          {articles.slice(0, newsLimit).map((article) => {
+            return <NewsCard key={article.url} article={article} />;
+          })}
+        </ul>
+        {newsLimit < articles.length && (
+          <button
+            onClick={handleClickButton}
+            type="button"
+            className="news__button"
+          >
+            Mostrar mais
+          </button>
+        )}
+      </>
+    );
+  }
+
   return (
     <>
-      <h2 className="news__title">Procurar resultados</h2>
-      <ul className="news__card-list">
-        {articles.slice(0, newsLimit).map((article) => {
-          return <NewsCard key={article.url} article={article} />;
-        })}
-      </ul>
-      {newsLimit < articles.length && (
-        <button
-          onClick={handleClickButton}
-          type="button"
-          className="news__button"
-        >
-          Mostrar mais
-        </button>
-      )}
+      <img className="news__not-found-image" src={notFound} alt="" />
+      <h2 className="news__not-found-title">Nada Encontrado</h2>
+      <p className="news__not-found-content">
+        Desculpe, mas nada corresponde aos seus termos de pesquisa.
+      </p>
     </>
   );
 }
