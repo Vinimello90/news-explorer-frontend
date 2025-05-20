20dbadd571ee3301f74c2b4433004b00cc77e6b9
vinimello90 <vinicius.barretto9022@gmail.com>
2025-05-20 00:27:08 -0300

    refactor(API): improve the search parameters

--- src/utils/thirdPartyApi.js
+++ src/utils/thirdPartyApi.js
@@ -1,6 +1,9 @@
+const dateFrom = new Date(Date.now() - 604800000).toISOString();
+const dateNow = new Date().toISOString();
+
 export function getNews(keyword) {
   return fetch(
-    `https://nomoreparties.co/news/v2/everything?q=${keyword}&language=pt&sortby=relevance&sortby=publishedat&apiKey=e8f08f2a6b4349fba83b1ead12f0a81f`
+    `https://nomoreparties.co/news/v2/everything?q=${keyword}&from=${dateFrom}&to=${dateNow}&sortby=relevancy&language=pt&pageSize=100&apiKey=e8f08f2a6b4349fba83b1ead12f0a81f`
   ).then((res) =>
     res.ok ? res.json() : Promise.reject(`Error: ${res.status}`)
   );
