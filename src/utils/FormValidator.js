592907197e566d95663c9287c345f3abb5f4da81
vinimello90 <vinicius.barretto9022@gmail.com>
2025-05-28 13:50:11 -0300

    feat(auth): improve password validation in FormValidator and prevent error text overflow with min-height

--- src/utils/FormValidator.js
+++ src/utils/FormValidator.js
@@ -31,19 +31,64 @@ export default class FormValidator {
     this._handleFormButtonState(hasInvalidInput);
   };
 
+  _isPasswordValid = (inputElement) => {
+    const value = inputElement.value;
+    const checkInput = [
+      { regex: /[A-Z]/, message: "1 letra maiúscula" },
+      { regex: /[a-z]/, message: "1 letra minúscula" },
+      { regex: /[0-9]/, message: "1 número" },
+      {
+        regex: /[!@#$%^&*()\-_=+[\]{};:,.<>?]/,
+        message: "1 caractere especial",
+      },
+      { regex: /.{8,}/, message: "mínimo de 8 caracteres" },
+    ];
+
+    const errors = checkInput
+      .filter(({ regex }) => !regex.test(value))
+      .map(({ message }) => message);
+
+    const message = `Precisa conter ${errors.join(", ")}.`;
+    return { isValid: errors.length === 0, message };
+  };
+
   // Válida os inputs dos formulários e retorna verdadeiro ou falso.
   _hasInvalidInput = (inputList) => {
     return inputList.some((inputElement) => {
+      const isSignup = inputElement.form.name === "signup";
+      if (inputElement.value && inputElement.type === "password" && isSignup) {
+        const validation = this._isPasswordValid(inputElement);
+        return !validation.isValid;
+      }
       return !inputElement.validity.valid;
     });
   };
 
-  // Válida os inputs para exibir ou esconder a mensagem de erro
-  validateInput = (inputElement) => {
+  // Válida o input de senha para exibir ou esconder a mensagem de erro
+  _validatePasswordInput = (inputElement) => {
+    const validation = this._isPasswordValid(inputElement);
+    if (!validation.isValid) {
+      this._showInputError(inputElement.id, validation.message);
+    } else {
+      this._hideInputError(inputElement.id);
+    }
+  };
+
+  _validateStandardInput = (inputElement) => {
     if (!inputElement.validity.valid) {
       this._showInputError(inputElement.id, inputElement.validationMessage);
     } else {
       this._hideInputError(inputElement.id);
+    }
+  };
+
+  // Válida os inputs para exibir ou esconder a mensagem de erro
+  validateInput = (inputElement) => {
+    const isSignup = inputElement.form.name === "signup";
+    if (inputElement.value && inputElement.type === "password" && isSignup) {
+      this._validatePasswordInput(inputElement);
+    } else {
+      this._validateStandardInput(inputElement);
     }
   };
 
